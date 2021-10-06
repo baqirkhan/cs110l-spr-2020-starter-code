45e2d68ad56b0f2796ede2c6e006d9853ac86911
baqir <baqirkhan00@gmail.com>
2021-10-06 13:44:15 +0530

    Add week - 1 part 3 exercises - Hangman

--- week1/part-3-hangman/src/main.rs
+++ week1/part-3-hangman/src/main.rs
@@ -27,14 +27,60 @@ fn pick_a_random_word() -> String {
     String::from(words[rand::thread_rng().gen_range(0, words.len())].trim())
 }
 
+fn get_input() -> char {
+    print!("Please guess a letter: ");
+    io::stdout()
+        .flush()
+        .expect("Error flushing stdout.");
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Error reading line.");
+    guess.trim().parse().unwrap()
+}
+
+fn process(guessed: &mut u8, remaining: &mut u32 ,current: & mut Vec<char>, letters: & mut Vec<char>, secret_word_chars:  &Vec<char>) {
+    println!("The word so far is {}", current.iter().collect::<String>());
+    println!("You have guessed the following letters: {}", letters.iter().collect::<String>());
+    println!("You have {} guesses left", remaining);
+    let input = get_input();
+    let mut has_guessed = false;
+    letters.push(input);
+
+    for (i,letter) in secret_word_chars.iter().enumerate() {
+        if *letter == input {
+            current[i] = *letter;
+            *guessed += 1;
+            has_guessed = true;
+        }
+    }
+
+    if !has_guessed {
+        *remaining -= 1;
+        println!("Sorry, that letter is not in the word");
+    }
+}
+
 fn main() {
     let secret_word = pick_a_random_word();
     // Note: given what you know about Rust so far, it's easier to pull characters out of a
     // vector than it is to pull them out of a string. You can get the ith character of
     // secret_word by doing secret_word_chars[i].
     let secret_word_chars: Vec<char> = secret_word.chars().collect();
+    let mut result = vec!['-'; secret_word_chars.len()];
+    let mut guessed_letters: Vec<char> = Vec::new();
+    let mut guessed: u8 = 0;
+    let mut guess_remaining = NUM_INCORRECT_GUESSES;
     // Uncomment for debugging:
-    // println!("random word: {}", secret_word);
-
+    println!("random word: {}", secret_word);
+    println!("Welcome to CS110L Hangman!");
     // Your code here! :)
+
+    loop {
+        process(& mut guessed, &mut guess_remaining, & mut result, & mut guessed_letters, &secret_word_chars);
+        if guessed == secret_word_chars.len() as u8 || guess_remaining == 0 {
+            break
+        }
+    }
+
 }
